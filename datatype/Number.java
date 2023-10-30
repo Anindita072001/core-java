f30b21cb8db14c9d0cef30dbc392066ca81bc9bc
Anindita Manna <104547030+Anindita072001@users.noreply.github.com>
2023-10-31 01:31:38 +0530

    Add files via upload

---
+++ datatype/Number.java
@@ -0,0 +1,18 @@
+package datatype;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num1 = 8.2;
+        int num2 = 6;
+
+        // Calculate the product
+        double product = num1 * num2;
+
+        // Print the product on the screen
+        System.out.println("Product of " + num1 + " and " + num2 + ": " + product);
+	}
+
+}
---
+++ datatype/ValuePrint.java
@@ -0,0 +1,17 @@
+package datatype;
+
+public class ValuePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6;
+        double b = 3.14;
+        char c = 'A';
+
+        // Print variables on the screen
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Character: " + c);
+	}
+
+}
