fb7409fbc9f49a5c72b4c8bb3eed8175d00dce3b
Anindita Manna <104547030+Anindita072001@users.noreply.github.com>
2023-10-31 01:26:56 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,17 @@
+package datatype;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int length = 2;
+	        int width = 3;
+
+	        // Calculate the area of the rectangle
+	        int area = length * width;
+
+	        // Print the area on the screen
+	        System.out.println("Area of the rectangle: " + area + " square units");
+	}
+
+}
